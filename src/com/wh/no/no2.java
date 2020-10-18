a9dc09e2c83dd4903a8b0d70920385a4d447faff
wanghaokfgz <982719779@qq.com>
2020-10-18 21:41:30 +0800

    no2

---
+++ src/com/wh/no/no2.java
@@ -0,0 +1,26 @@
+package com.wh.no;
+
+/**
+ * @author wanghao
+ * @date 2020/10/18 0018 21:34
+ * @discriptiong 2. 两数相加
+ */
+public class no2 {
+}
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+    }
+}
