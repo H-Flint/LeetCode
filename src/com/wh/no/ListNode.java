e0ac7d0c5500b48411e49ca1b3dd2e9be65ea2e1
wanghaokfgz <982719779@qq.com>
2020-10-18 22:47:44 +0800

    完成整数int的处理，忽略了长度问题

---
+++ src/com/wh/no/ListNode.java
@@ -0,0 +1,14 @@
+package com.wh.no;
+
+/**
+ * @author wanghao
+ * @date 2020/10/18 0018 22:03
+ * @discription
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
